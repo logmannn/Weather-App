dfe71314ea1af0ef604a581aa1ed60d9709ab73d
lt <logantanous@gmail.com>
2018-08-13 19:40:14 -0500

    use callback to pass which day was selected and the current daily hours of selected

--- src/App.js
+++ src/App.js
@@ -6,13 +6,40 @@ import "./App.css";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      dailyHours: ""
+    };
+    this.handler = this.handler.bind(this);
+  }
+
+  handler(dailyHours) {
+    // event.preventDefault();
+    // console.log(dailyHours);
+    // console.log(this.props);
+    this.setState({
+      dailyHours: dailyHours
+    });
+    // console.log(this.state);
   }
 
   render() {
     const cards = [];
     const weather = this.props.weather;
+    let selected = "";
+
     this.props.weather.forEach(weather => {
-      cards.push(<WeatherCard weather={weather} key={weather.dayOfTheWeek} />);
+      if (this.state.dailyHours[0] == weather.dayOfTheWeek) {
+        selected = weather.dayOfTheWeek;
+      }
+
+      cards.push(
+        <WeatherCard
+          weather={weather}
+          key={weather.dayOfTheWeek}
+          handler={this.handler.bind(this)}
+          selected={selected}
+        />
+      );
     });
 
     return <div className="App">{cards}</div>;
--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -3,16 +3,34 @@ import React, { Component } from "react";
 class WeatherCard extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      dailyHours: this.props.weather.hourly,
+      dayOfTheWeek: this.props.weather.dayOfTheWeek
+    };
   }
+
+  handler() {
+    this.props.handler([this.state.dayOfTheWeek, this.state.dailyHours]);
+  }
+
   render() {
     const dayOfTheWeek = this.props.weather.dayOfTheWeek;
     const lowWeather = this.props.weather.highLow[1];
     const highWeather = this.props.weather.highLow[0];
     const weather = this.props.weather.weather;
+    const hourly = this.props.weather.hourly;
+
+    let selected = "";
+
+    if (dayOfTheWeek == this.props.selected) {
+      selected = "selected";
+    }
 
     return (
-      <div className="weatherCard">
+      <div
+        className={"weatherCard " + selected}
+        onClick={this.handler.bind(this)}
+      >
         <div className="dayOfTheWeek">{dayOfTheWeek}</div>
         <div className="imageWrapper">
           <img src={require("./images/" + weather + ".png")} />
